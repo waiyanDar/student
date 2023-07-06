727faaa818320692f6a943c9ebb73baa9dd92fd3
waiyanDar <waiyan.f3lix@gmail.com>
2023-07-06 17:03:13 +0630

    student

--- src/main/java/com/example/student/register/security/GlobalMethodSecurityConfig.java
+++ src/main/java/com/example/student/register/security/GlobalMethodSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.student.register.security;
 
+import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 public class GlobalMethodSecurityConfig extends GlobalMethodSecurityConfiguration {
@@ -10,4 +13,13 @@ public class GlobalMethodSecurityConfig extends GlobalMethodSecurityConfiguratio
 	public GlobalMethodSecurityConfig(RoleHierarchy roleHierarchy) {
 		this.roleHierarchy = roleHierarchy;
 	}
+
+	@Override
+	protected AccessDecisionManager accessDecisionManager() {
+		AffirmativeBased affirmativeBased = (AffirmativeBased) super.accessDecisionManager();
+		affirmativeBased.getDecisionVoters().add(new RoleHierarchyVoter(roleHierarchy));
+		return affirmativeBased;
+	}
+	
+	
 }
